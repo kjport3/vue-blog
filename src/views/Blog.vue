582d734ba69ee7b768c201d3ed153efa94881a1a
Kenneth Porterfield <kjport3@gmail.com>
2021-07-15 15:04:39 -0400

    vue blog work

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,15 +1,26 @@
 <template>
     <h1>Blog time</h1>
+    <BlogShowcase />
+    <div></div>
+    
 </template>
 
 <script>
+import BlogShowcase from '../components/BlogShowcase.vue'
+
 export default {
-    name: 'Blog'
+    name: 'Blog',
+    components: {
+        BlogShowcase
+    }
 }
 </script>
 
 <style scoped>
 h1 {
     text-align: center;
+}
+div {
+    height:300px;
 }
 </style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Contact Me</h1>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'Contact'
+}
+</script>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+div {
+    height: 500px;
+}
+</style>
